79d829aa04d83285c90e5a91be9e305d35f5b830
Ismar Hadzic <hadzicismar67@gmail.com>
2018-04-24 08:23:48 -0600

    Download Event
    Finshed with download event, show download link

--- data/js/main.js
+++ data/js/main.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
   //
 
-  function download(url) {
-
-    ytdl(url, { filter: (format) => format.container === 'mp3' })
-  .pipe(fs.createWriteStream('video.mp3'));
-
-  }
-
   $('form').on('submit', function(e) {
     e.preventDefault();
     var videolink = $('input[name=video]').val();
     console.log(videolink);
-    
+
     if(!videolink) {
       console.log("No video link!");
     }
@@ -24,22 +17,15 @@ $(document).ready(function() {
         throw err;
       }
 
-      $('#results').append("<li>"+info.title+" - <a href='/downloads/"+info.video_id+".mp4' download>Download </a></li>");
-
       var download = ytdl.downloadFromInfo(info, { filter: (format) => format.container === 'mp4' });
 
       download.pipe(fs.createWriteStream('downloads/'+info.video_id+'.mp4'));
 
-      download.once('response', () => {
-        starttime = Date.now();
-      });
+      download.on('finish', () => {
+        console.log('All downloaded!');
 
-      download.on('progress', function(chunkLength, downloaded, total) {
-        var floatDownloaded = downloaded/total;
-        var downloadedMinutes = ( Date.now() - starttime ) / 1000 / 60;
-
-        console.log(downloadedMinutes / floatDownloaded - downloadedMinutes)
-      });
+        $('#results').append("<li>"+info.title+" - <a href='/downloads/"+info.video_id+".mp4' download>Download </a></li>");
+      })
 
     });
 
