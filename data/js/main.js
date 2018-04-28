7e94d9e9b322bd6ab52e93f9c7fdc3494358559e
Ismar Hadzic <hadzicismar67@gmail.com>
2018-04-28 01:55:41 -0600

    Audio Support
    Audio conversion support with minor fixes on progress and quality..

--- data/js/main.js
+++ data/js/main.js
@@ -15,44 +15,70 @@ $(document).ready(function() {
       console.log("No video link!");
     }
 
-    $.when(ytdl.getInfo(videolink)).then(function(data) {
+    if (informat == 'audioonly') {
 
-      console.log("info callback then do smth");
-      console.log(data);
+      console.log('audio only, mp3 file');
 
-      console.log("using filter format: " + informat );
+      $.when(ytdl.getInfo(videolink)).then(function(data) {
 
-      var video = ytdl(videolink, { filter: (format) => format.container === informat });
+        var audio = ytdl(videolink, { filter:format => { return format.container === 'm4a' && !format.encoding; } });
 
-      video.pipe(fs.createWriteStream('downloads/'+data.video_id+'.'+informat));
-
-      console.log('Output to: downloads/'+data.video_id+'.'+informat);
-
-      video.once('response', () => {
-        starttime = Date.now();
-        console.log("Download started at: " + starttime);
-      });
-
-      video.on('progress', (chunkLength, downloaded, total) => {
-        const floatDownloaded = downloaded / total;
-        const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
-        readline.cursorTo(process.stdout, 0);
-
-        console.log((floatDownloaded*100).toFixed(2) + '% downloaded!');
-        console.log((downloaded / 1024 / 1024).toFixed(2) + 'MB of ' + (total / 1024 / 1024).toFixed(2) + 'MB ');
-        console.log((downloadedMinutes/floatDownloaded - downloadedMinutes).toFixed(2) + "minutes");
-        var bar_value = floatDownloaded * 35;
-        $('.progress-bar ').css({width: bar_value +"em"});
-        readline.moveCursor(process.stdout, 0, -1);
-      });
-
-      video.on('end', () => {
-        $('#results').append("<li>"+data.title+" - <a href='downloads/"+data.video_id+"."+informat+"' download>Save</a></li>");
-        $('.progress-bar').css({width: '0em'});
-        console.log('End of file write stream.');
-      });
+        audio.pipe(fs.createWriteStream('downloads/' + data.video_id + '.mp3'));
+        audio.once('response', () => {
+          starttime = Date.now();
+          console.log("Download started at: " + starttime);
+        });
+        audio.on('progress', (chunkLength, downloaded, total) => {
+          const floatDownloaded = downloaded / total;
+          const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+          readline.cursorTo(process.stdout, 0);
+          console.log((floatDownloaded*100).toFixed(2) + '% downloaded!');
+          console.log((downloaded / 1024 / 1024).toFixed(2) + 'MB of ' + (total / 1024 / 1024).toFixed(2) + 'MB ');
+          console.log((downloadedMinutes/floatDownloaded - downloadedMinutes).toFixed(2) + "minutes");
+          var bar_value = floatDownloaded * 35;
+          $('.progress-bar ').css({width: bar_value +"em"});
+          readline.moveCursor(process.stdout, 0, -1);
+        });
+        audio.on('end', () => {
+          $('#results').append("<li>"+data.title+" - <a href='downloads/"+data.video_id+".mp3' download='"+data.title+".mp3' >Save</a></li>");
+          $('.progress-bar').css({width: '0em'});
+          console.log('End of file write stream.');
+        });
 
     });
+
+    } else {
+
+      $.when(ytdl.getInfo(videolink)).then(function(data) {
+
+        console.log("using filter format: " + informat );
+
+        var video = ytdl(videolink, { filter: (format) => format.container === informat });
+        video.pipe(fs.createWriteStream('downloads/'+data.video_id+'.'+informat));
+        console.log('Output to: downloads/'+data.video_id+'.'+informat);
+        video.once('response', () => {
+          starttime = Date.now();
+          console.log("Download started at: " + starttime);
+        });
+        video.on('progress', (chunkLength, downloaded, total) => {
+          const floatDownloaded = downloaded / total;
+          const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+          readline.cursorTo(process.stdout, 0);
+          console.log((floatDownloaded*100).toFixed(2) + '% downloaded!');
+          console.log((downloaded / 1024 / 1024).toFixed(2) + 'MB of ' + (total / 1024 / 1024).toFixed(2) + 'MB ');
+          console.log((downloadedMinutes/floatDownloaded - downloadedMinutes).toFixed(2) + "minutes");
+          var bar_value = floatDownloaded * 35;
+          $('.progress-bar ').css({width: bar_value +"em"});
+          readline.moveCursor(process.stdout, 0, -1);
+        });
+        video.on('end', () => {
+          $('#results').append("<li>"+data.title+" - <a href='downloads/"+data.video_id+"."+informat+"' download='"+data.title+"."+informat+"' >Save</a></li>");
+          $('.progress-bar').css({width: '0em'});
+          console.log('End of file write stream.');
+        });
+
+      });
+    }
 
   });
 
